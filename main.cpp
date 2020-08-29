1ad1e9dc3108d5837645ed00e08425d67dc725f7
AndyNgoIT <70431419+AndyNgoIT@users.noreply.github.com>
2020-08-29 16:18:25 +0700

    Add files via upload

---
+++ main.cpp
@@ -0,0 +1,258 @@
+#include <iostream>
+#include <windows.h>
+using namespace std;
+
+int main()
+{
+    int i,j;
+    for (i=0;i<=100;i++) {
+        system("cls");
+        cout <<"\n\t\t\tConnected to Love Server...";
+        cout << "\n\n\t\t\tSend ...::... to my love !";
+        cout << "\n\n\t\t__________________________________________";
+        cout << "\n\n\n\t\t\t\tLoading   "<<i<<" %"<<endl;
+    if (i==10) { cout << "\n\n\t\t\tT            " ;
+                    cout <<"\n\n\n\t\tTO CO DIEU MUON NOI VOI CAU.CAU HAY LANG NGHE NHE!!!   ";
+                    Sleep(4000);          }
+                    if (i==20) {cout <<"\n\n\n\t\tLoading....";
+                    Sleep(3000); }
+    if (i==30) {
+        cout << "\n\n\t\t\tTO                    ";
+        cout << "\n\n\n\t\tNgay tu khi gap cau to da bi cam nang cau mat roi. \n\t\tTo da lo nhu chung ta da tung gap nhau vay!";
+        Sleep(4000);
+    }
+    if (i==40) {
+        cout<< "\n\n\t\t\tTO   Y                  ";
+        cout << "\n\n\n\t\tTu khi gap cau trai tim to luc nao cung loan nhip. \n\t\tHinh nhu cau da danh cap trai tim to roi!";
+        Sleep(4000);
+    }
+    if (i==50) {
+        cout << "\n\n\t\t\tTO   YE               ";
+        cout << "\n\n\n\t\tCau co biet khong? Luc nao to cung nghi ve cau thoi! \n\t\tHinh nhu cau la me tuong lai cua cac con to thi phai.";
+        Sleep(5000);
+    }
+    if (i==60) {
+        cout << "\n\n\t\t\tTO    YEU            ";
+        cout << "\n\n\n\t\tTren the gioi nay co 7 ty nguoi \n\t\tMa sao chi co 1 nguoi yeu cau.";
+        cout << "\n\t\t==>VA NGUOI DO CHINH LA TO DAY !!!";
+        Sleep(5000);
+    }
+    if (i==70) {
+        cout << "\n\n\t\t\tTO    YEU    C       ";
+        cout << "\n\n\n\t\tTo la dan ky thuat nen to khong co nhieu van hay,\n\t\t khong lang man nhu nguoi ta. \n\t\tNhung to se bao ve va che cho den het cuoc doi!";
+        Sleep(4000);
+    }
+    if (i==80) {
+        cout << "\n\n\t\t\tTO    YEU    CA       ";
+        cout << "\n\n\n\t\tTo hoc Lap trinh huong doi tuong \n\t\tnhung doi tuong ma to huong den chi co cau ma thoi!  \nS2";
+        Sleep(4000);
+    }
+    if (i==90) {
+        cout << "\n\n\t\t\tTO    YEU    CAU       ";
+        cout << "\n\n\n\t\tHAY LAM NGUOI YEU TO NHE ! TO THICH CAU MAT ROI !  \n\t\tS2";
+        Sleep(5000);
+    }
+    if (i==98) {
+        cout << "\n\n\t\t\tTO    YEU    CAU   !    ";
+        cout << "\n\n\n\t\tVa To Khong The Song Thieu Cau! Boi Vi To Da Lo Yeu Cau Mat Roi!"<<endl;
+        Sleep(5000);
+    }
+    if (i==99) {
+        for (j=1;j<=23;j++) {
+            system("cls");
+            if (j==1) {
+                cout << "\t\t *                   \n";
+              Sleep(500);
+            }
+            if (j==2) {
+                cout << "\t\t * .                 \n";
+            Sleep(500);
+            }
+            if (j==3) {
+                cout << "\t\t * .                  \n";
+                cout << "\t\t      .                \n";
+                Sleep(500);
+            }
+            if (j==4) {
+                cout << "\t\t * .     .             \n";
+                cout << "\t\t      .                \n";
+                Sleep(500);
+            }
+            if (j==5) {
+                cout << "\t\t * .     . *           \n";
+                cout << "\t\t      .                \n";
+                Sleep(500);
+            }
+            if (j==6){
+                cout << "\t\t * .     . * .           \n";
+                cout << "\t\t      .                  \n";
+                Sleep(500);
+            }
+            if (j==7) {
+                cout << "\t\t * .     . * .           \n";
+                cout << "\t\t      .      *           \n";
+                Sleep(500);
+            }
+            if (j==8) {
+                cout << "\t\t * .     . * .           \n";
+                cout << "\t\t      .      *           \n";
+                cout << "\t\t            *           \n";
+                Sleep(500);
+            }
+            if (j==9) {
+                cout << "\t\t * .     . * .            \n";
+                cout << "\t\t      .      *           \n";
+                cout << "\t\t            *           \n";
+                cout << "\t\t           *            \n";
+                Sleep(500);
+            }
+            if (j==10) {
+                cout << "\t\t * .     . * .           \n";
+                cout << "\t\t      .      *         \n";
+                cout << "\t\t            *          \n";
+                cout << "\t\t           *            \n";
+                cout << "\t\t          *             \n";
+                Sleep(500);
+            }
+            if (j==11){
+                cout << "\t\t * .     . * .           \n";
+                cout << "\t\t      .      *          \n";
+                cout << "\t\t            *          \n";
+                cout << "\t\t           *            \n";
+                cout << "\t\t          *             \n";
+                cout << "\t\t         *               \n";
+                Sleep(500);
+            }
+            if (j==12) {
+                cout << "\t\t * .     . * .           \n";
+                cout << "\t\t      .      *          \n";
+                cout << "\t\t            *          \n";
+                cout << "\t\t           *            \n";
+                cout << "\t\t          *             \n";
+                cout << "\t\t         *               \n";
+                cout << "\t\t       *                 \n";
+                Sleep(500);
+            }
+            if (j==13) {
+                cout << "\t\t * .     . * .           \n";
+                cout << "\t\t      .      *          \n";
+                cout << "\t\t            *          \n";
+                cout << "\t\t           *            \n";
+                cout << "\t\t   *      *             \n";
+                cout << "\t\t     *   *               \n";
+                cout << "\t\t       *                 \n";
+                Sleep(500);
+            }
+            if (j==14){
+                cout << "\t\t * .     . * .           \n";
+                cout << "\t\t      .      *           \n";
+                cout << "\t\t            *            \n";
+                cout << "\t\t *         *             \n";
+                cout << "\t\t   *      *              \n";
+                cout << "\t\t     *   *               \n";
+                cout << "\t\t       *                 \n";
+                Sleep(500);
+            }
+            if (j==15) {
+                cout << "\t\t * .     . * .          \n";
+                cout << "\t\t      .      *          \n";
+                cout << "\t\t*           *           \n";
+                cout << "\t\t *         *            \n";
+                cout << "\t\t   *      *             \n";
+                cout << "\t\t     *   *              \n";
+                cout << "\t\t       *                \n";
+                Sleep(500);
+            }
+            if (j==16) {
+                cout <<"\t\t    . * .     . * .           \n";
+                cout <<"\t\t   *      .       *           \n";
+                cout <<"\t\t   *              *           \n";
+                cout <<"\t\t     *           *            \n";
+                cout << "\t\t      *        *             \n";
+                cout << "\t\t        *     *              \n";
+                cout << "\t\t           *                 \n";
+                Sleep(500);
+            }
+            if (j==17) {
+                cout << "\t\t. * .     . * .                       . * .     . * .                   \n";
+               cout <<  "\t\t*     .       *                     *       .       *                 \n";
+                cout << "\t\t*             *                      *              *                   \n";
+                cout << "\t\t*   To        *     YEU               *    CAU   *                    \n";
+                cout << "\t\t  *        *                            *        *                     \n";
+                cout << "\t\t    *    *                                *    *                      \n";
+                cout << "\t\t       *                                     *                         \n";
+                Sleep(500);
+            }
+            if (j==18) {
+                cout << "\t\t                                                                       \n";
+                cout << "\t\t                                                                       \n";
+                cout << "\t\t                                                                       \n";
+                cout << "\t\t     To             YEU                          CAU                           \n";
+                cout << "\t\t                                                                       \n";
+                cout << "\t\t                                                                       \n";
+                cout << "\t\t                                                                       \n";
+                Sleep(500) ;
+            }
+            if (j==19) {
+                cout << "\t\t. * .     . * .                       . * .     . * .                   \n";
+               cout <<  "\t\t*     .       *                     *       .       *                 \n";
+                cout << "\t\t*             *                      *              *                   \n";
+                cout << "\t\t*   To        *     YEU               *    CAU   *                    \n";
+                cout << "\t\t  *        *                            *        *                     \n";
+                cout << "\t\t    *    *                                *    *                      \n";
+                cout << "\t\t       *                                     *                         \n";
+                Sleep(500);
+            }
+            if (j==20) {
+                cout << "\t\t                                                                       \n";
+                cout << "\t\t                                                                       \n";
+                cout << "\t\t                                                                       \n";
+                cout << "\t\t     To             YEU                          CAU                           \n";
+                cout << "\t\t                                                                       \n";
+                cout << "\t\t                                                                       \n";
+                cout << "\t\t                                                                       \n";
+                Sleep(500) ;
+            }
+            if (j==21) {
+                cout << "\t\t. * .     . * .                       . * .     . * .                   \n";
+                cout <<  "\t\t*     .       *                     *       .       *                 \n";
+                cout << "\t\t*              *                     *              *                   \n";
+                cout << "\t\t*   To        *     YEU               *    CAU     *                    \n";
+                cout << "\t\t  *        *                            *        *                     \n";
+                cout << "\t\t    *    *                                *    *                      \n";
+                cout << "\t\t       *                                     *                         \n";
+                Sleep(500);
+            }
+            if (j==22) {
+                cout << "\t\t                                                                       \n";
+                cout << "\t\t                                                                       \n";
+                cout << "\t\t                                                                       \n";
+                cout << "\t\t     To             YEU                          CAU                           \n";
+                cout << "\t\t                                                                       \n";
+                cout << "\t\t                                                                       \n";
+                cout << "\t\t                                                                       \n";
+                Sleep(500) ;
+            }
+            if (j==23) {
+                cout << "\t\t. * .     . * .                       . * .     . * .                   \n";
+                cout <<  "\t\t*     .       *                     *       .       *                 \n";
+                cout << "\t\t*              *                     *              *                   \n";
+                cout << "\t\t*   To        *     YEU               *    CAU   *                    \n";
+                cout << "\t\t  *        *                            *        *                     \n";
+                cout << "\t\t    *    *                                *    *                      \n";
+                cout << "\t\t       *                                     *                         \n";
+                Sleep(500);
+            }
+        }
+        Sleep(3000);
+    }
+    if (i==100) {
+        cout << "\n\n\t\tTo yeu cau.. ::  100% yeu cau!!!";
+        cout << "\n\n\n\t\t\t------------ MAI MAI YEU CAU --------------\n\n\n\n\n"<<endl;
+        Sleep(5000);
+    }
+    Sleep(100);
+    }
+    cout << "\n\n\n\n\t\t\t------Made By Dong Ngo-------    "<< "\n\n\t\t\tCopyright by Dong Ngo   \n\n\n "<<endl;
+    system("pause") ;
+}
